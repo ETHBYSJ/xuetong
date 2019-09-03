2df01e69bf5336d61010ec65d054b2c202a03f00
shuishuimeng <981904597@qq.com>
2019-09-04 06:15:18 +0800

    教师端-我的-学情反馈 初版学生列表

--- pages/member/studentList/studentList.js
+++ pages/member/studentList/studentList.js
@@ -1,4 +1,5 @@
 let we = require('../../../we/index.js')
+let py = require('../../../utils/pinyin.js')
 
 new class extends we.Page {
   data() {
@@ -6,10 +7,12 @@ new class extends we.Page {
       vo: {
         //'BackgroundImg': 'http://pic.616pic.com/bg_w1180/00/11/52/pKsDC2YqzS.jpg!/fh/300',
         message: {},
-        stuList: [],
+        
       },
       userType: "",
       stuList: [],
+      sortList: [],
+      isActive: null,
     }
   }
 
@@ -17,7 +20,6 @@ new class extends we.Page {
     this.setData({
       'userType': this.$app.userType,
       'vo.imgBaseUrl': this.$app.imgBaseUrl,
-      
     })
 
     if (this.$app.userType == '教职工') {
@@ -27,39 +29,104 @@ new class extends we.Page {
     }
   }
 
-
-  loadStuInfo(gradeId, clazzId) {
-    this.$get('/v1/student/datalist?gradeId=' + gradeId +'&clazzId='+ clazzId).then(data => {
-      data.obj.forEach(e => {
-        this.data.vo.stuList.push({
-          'name': e.name,
-          'photo': e.photo,
-        });
-        this.setData({
-          'stuList': this.data.vo.stuList, 
-        })
-      })
+  //点击学生转到新页面
+  bindStudy(e) {
+    wx.navigateTo({
+      url: '/pages/member/studentStudy/studentStudy?id=' + e.target.dataset.id + '&name=' + e.target.dataset.name,
     })
   }
 
+  //点击右边导航字母
+  scrollToViewFn(e) {
+    this.setData({
+      'isActive': e.target.dataset.id,
+    });
+  }
+  
   loadTechInfo() {
-    this.$get('/v1/teacher/getInfo').then(data => {
+    var ps1 = this.$get('/v1/teacher/getInfo').then(data => {
       this.setData({
         'vo.message': data.obj
       })
-
+      //塞入队列
+      let http_list = [];
       for (let i = 0; i < data.obj.webchatClazzList.length; ++i) {
-        this.loadStuInfo(data.obj.webchatClazzList[i].gradeid,
-                         data.obj.webchatClazzList[i].clazzid);
+        let gradeId = data.obj.webchatClazzList[i].gradeid;
+        let clazzId = data.obj.webchatClazzList[i].clazzid;
+        http_list.push(new Promise((resolve, reject)=>{
+          this.$get('/v1/student/datalist?gradeId=' + gradeId + '&clazzId=' + clazzId).then(res => {
+            resolve(res.obj)
+          }).catch(err => {
+            reject(err)
+          })
+        }));
       }
+
+      Promise.all(http_list).then(values => {
+        let stuList = [];
+        for(let index in values) {
+          stuList = stuList.concat(values[index]);
+        }
+        this.setData({
+          'stuList': stuList,
+        })
+
+        return Promise.resolve(stuList);
+
+      }).then(stuList => { //初始化排序队列
+        let tmp = stuList;
+        let sor = [];
+        for (let i = 0; i < 27; ++i) {
+          if (i < 26) {
+            sor.push({
+              'id': i,
+              'active': false,
+              'region': String.fromCharCode(i + 65),
+              'items': [],
+            })
+          } else {
+            sor.push({
+              'active': false,
+              'region': "#",
+              'items': [],
+            })
+          }
+        }
+        //提取首字母
+        tmp.forEach(e => {
+          let head_char = py.pinyin.getCamelChars(e.name).toUpperCase()[0];
+          if (head_char == undefined || head_char > 'Z' || head_char < 'A') {
+            sor[26].items.push(e);
+            sor[26].active = true;
+          } else {
+            let ascii = head_char.charCodeAt();
+            sor[ascii - 65].items.push(e);
+            sor[ascii - 65].active = true;
+          }
+        });
+
+        return Promise.resolve(sor);
+      }).then(sor => {
+        this.setData({
+          'sortList': sor,
+        });
+        console.log(sor);
+      }).catch(err => {
+        this.$showModal({
+          title: '获取信息错误',
+          content: err.msg,
+          showCancel: false
+        })
+      })
       //console.log(data);
-    }).catch(err => {
+    }).catch (err => {
       this.$showModal({
         title: '获取信息错误',
         content: err.msg,
         showCancel: false
       })
     })
+    
   }
 
   loadInfo() {
---
+++ pages/member/studentStudy/studentStudy.js
@@ -0,0 +1,88 @@
+let we = require('../../../we/index.js')
+new class extends we.Page {
+  data() {
+    return {
+      disabled: false,
+      code: '获取验证码',
+      po: {
+        
+      },
+      vo: {
+        message: {},
+        
+      },
+      userType: '',
+      student_name: '',
+      student_id: '',
+    }
+  }
+
+  onLoad(options) {
+    this.setData({
+      'student_name': options.name,
+      'student_id'  : options.id,
+    });
+
+  }
+
+  onShow() {
+    this.setData({
+      'userType': this.$app.userType
+    })
+
+    if (this.$app.userType == '教职工') {
+      this.loadTechInfo()
+    } else {
+      this.loadInfo()
+    }
+
+
+  }
+
+  //创建新的学情反馈
+  bindNewStudy() {
+    wx.navigateTo({
+      url: '/pages/member/createStudy/createStudy',
+    })
+  }
+
+  loadTechInfo() {
+    this.$get('/v1/teacher/getInfo').then(data => {
+      this.setData({
+        'vo.message': data.obj
+      })
+    }).catch(err => {
+      this.$showModal({
+        title: '获取信息错误',
+        content: err.msg,
+        showCancel: false
+      })
+    })
+  }
+
+  loadInfo() {
+    this.$get('/v1/family/getInfo').then(data => {
+      this.setData({
+        'vo.message': data.obj
+      })
+      if (data.obj.phone) {
+        var tmp_phone = data.obj.phone;
+        tmp_phone = tmp_phone.slice(0, 3) + "****" + tmp_phone.slice(7);
+        this.setData({
+          'vo.phone_display': tmp_phone,
+        })
+      } else {
+        this.setData({
+          'vo.phone_display': null,
+        })
+      }
+      console.log(data);
+    }).catch(err => {
+      this.$showModal({
+        title: '获取信息错误',
+        content: err.msg,
+        showCancel: false
+      })
+    })
+  }
+}
